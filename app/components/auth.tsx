16946cb351903ea5d371a913c9e07626328f6a52
kurt.loong <kurt.loong@foxmail.com>
2023-08-11 15:08:11 +0800

    feat:添加登陆功能

--- app/components/auth.tsx
+++ app/components/auth.tsx
@@ -1,18 +1,54 @@
 import styles from "./auth.module.scss";
 import { IconButton } from "./button";
-
+import { useEffect, useState } from 'react';
 import { useNavigate } from "react-router-dom";
 import { Path } from "../constant";
 import { useAccessStore } from "../store";
 import Locale from "../locales";
-
 import BotIcon from "../icons/bot.svg";
 
 export function AuthPage() {
   const navigate = useNavigate();
   const access = useAccessStore();
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+  const [error, setError] = useState(null);
+
+  // 1. Check if user is already authorized, if so redirect to home
+  useEffect(() => {
+    if (access.isAuthorized()) {
+      goHome();
+    }
+  }, [access]);
 
   const goHome = () => navigate(Path.Home);
+
+  // Mock API call for user authentication
+  const authenticateUser = (username:string, password:string) => {
+    return new Promise((resolve, reject) => {
+      setTimeout(() => {
+        if (username === "admin" && password === "password123") {
+          resolve({ success: true });
+        } else {
+          reject({ success: false, message: "Invalid username or password." });
+        }
+      }, 1000);
+    });
+  };
+
+  const handleLogin = () => {
+    authenticateUser(username, password)
+      .then(response => {
+        if (response) {
+          // 3. After login, update the token or access code to simulate setting an auth state
+          access.updateToken('user_token'); // you can set a real token here
+          goHome();
+        }
+      })
+      .catch(error => {
+        setError(error.message);
+      });
+  };
 
   return (
     <div className={styles["auth-page"]}>
@@ -26,18 +62,26 @@ export function AuthPage() {
       <input
         className={styles["auth-input"]}
         type="text"
-        placeholder={Locale.Auth.Input}
-        value={access.accessCode}
-        onChange={(e) => {
-          access.updateCode(e.currentTarget.value);
-        }}
+        placeholder="Username"
+        value={username}
+        onChange={(e) => setUsername(e.target.value)}
       />
+
+      <input
+        className={styles["auth-input"]}
+        type="password"
+        placeholder="Password"
+        value={password}
+        onChange={(e) => setPassword(e.target.value)}
+      />
+
+      {error && <div className={styles["auth-error"]}>{error}</div>}
 
       <div className={styles["auth-actions"]}>
         <IconButton
           text={Locale.Auth.Confirm}
           type="primary"
-          onClick={goHome}
+          onClick={handleLogin}
         />
         <IconButton text={Locale.Auth.Later} onClick={goHome} />
       </div>
